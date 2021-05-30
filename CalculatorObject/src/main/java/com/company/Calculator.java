e942b5ebb6db1f7de75ddb8106062badfad24045
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-05-30 19:21:50 -0400

    created a new calculator class to hold methods, renamed old CalulcatorApplication to just App and instantiated the calculator object and removed all calculator methods since they were moved to the calculator class. Created calculator test and made sure they all pass.

--- CalculatorObject/src/main/java/com/company/Calculator.java
+++ CalculatorObject/src/main/java/com/company/Calculator.java
@@ -2,21 +2,6 @@ package com.company;
 
 public class Calculator {
 
-    public static void main(String[] args) {
-
-        Calculator calc = new Calculator();
-
-        System.out.println("1 + 1 = " + calc.addInt(1,1));
-        System.out.println("23 - 52 = " + calc.subtractInt(23,52));
-        System.out.println("34 * 2 = " + calc.multiplyInt(32,2));
-        System.out.println("12 / 3 = " + calc.divideInt(12,3));
-        System.out.println("12 / 7 = " + calc.divideDouble(12,7));
-        System.out.println("3.4 + 2.3 = " + String.format("%.2f", calc.addDouble(3.4,2.3)));
-        System.out.println("6.7 * 4.4 = " + String.format("%.2f",calc.multiplyDouble(6.7,4.4)));
-        System.out.println("5.5 - 0.5 = " + calc.subtractDouble(5.5,0.5));
-        System.out.println("10.8 / 2.2 = " + calc.divideDouble(10.8,2.2));
-    }
-
 //    Methods to calculate using two integer parameters
     public int addInt(int a, int b) {
         return a + b;
@@ -35,7 +20,7 @@ public class Calculator {
     }
 
 
-//    Methods to calculate using two double parameters
+    //    Methods to calculate using two double parameters
     public double addDouble(double a, double b) {
         return a + b;
     }
@@ -51,5 +36,4 @@ public class Calculator {
     public double divideDouble(double a, double b) {
         return a / b;
     }
-
 }
---
+++ CalculatorObject/src/test/java/com/company/CalculatorTest.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    Calculator calc;
+
+    @Before
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void shouldAddTwoIntegersAndReturnResult() {
+        int whatIShouldGet = 2;
+        int whatIGot = calc.addInt(1,1);
+        assertEquals(whatIShouldGet,whatIGot);
+    }
+
+    @Test
+    public void shouldSubtractTwoIntegersAndReturnResult() {
+        int whatIShouldGet = -29;
+        int whatIGot = calc.subtractInt(23,52);
+        assertEquals(whatIShouldGet,whatIGot);
+    }
+
+    @Test
+    public void shouldMultiplyTwoIntegersAndReturnResult() {
+        int whatIShouldGet = 64;
+        int whatIGot = calc.multiplyInt(32,2);
+        assertEquals(whatIShouldGet,whatIGot);
+    }
+
+    @Test
+    public void shouldDivideTwoIntegersAndReturnResult() {
+        int whatIShouldGet = 4;
+        int whatIGot = calc.divideInt(12,3);
+        assertEquals(whatIShouldGet,whatIGot);
+    }
+
+    @Test
+    public void shouldDivideTwoDoublesAndReturnResult() {
+        double whatIShouldGet = 1.7142857142;
+        double whatIGot = calc.divideDouble(12,7);
+        assertEquals(whatIShouldGet,whatIGot, 10);
+    }
+
+    @Test
+    public void shouldAddTwoDoublesAndReturnResult() {
+        double whatIShouldGet = 5.6999999999;
+        double whatIGot = calc.addDouble(3.4,2.3);
+        assertEquals(whatIShouldGet,whatIGot, 10);
+    }
+
+    @Test
+    public void shouldMultiplyTwoDoublesAndReturnResult() {
+        double whatIShouldGet = 29.4800000000;
+        double whatIGot = calc.multiplyDouble(6.7,4.4);
+        assertEquals(whatIShouldGet,whatIGot, 10);
+    }
+
+    @Test
+    public void shouldSubtractTwoDoublesAndReturnResult() {
+        double whatIShouldGet = 5.5;
+        double whatIGot = calc.subtractDouble(5.5,0.5);
+        assertEquals(whatIShouldGet,whatIGot,10);
+    }
+}
